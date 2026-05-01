7b326d6540b8a6ab70abd735801146a10d362db5
razzer1114 <58135048+razzer1114@users.noreply.github.com>
2026-05-02 07:10:51 +0800

    Update app_testing.py

--- app_testing.py
+++ app_testing.py
@@ -351,7 +351,7 @@ if item_type == "Custom / 自定义":
         min_value=0.01,
         max_value=0.99,
         value=0.50,
-        step=0.005
+        step=0.01
     )
 else:
     soul_success_rate = preset_map[item_type]
@@ -365,7 +365,7 @@ bless_relative_cost = st.sidebar.slider(
     min_value=0.50,
     max_value=15.00,
     value=5.29,
-    step=0.01
+    step=0.005
 )
 
 st.sidebar.header("曲线设置 Curve Settings")
